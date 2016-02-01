34cd216205ff5f508530e330ec12d1fd3c0fcd79
Edwin Salvador <chalosalvador@gmail.com>
2016-01-31 21:54:40 -0500

    index cover

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                 <ul class="nav masthead-nav">
                   <li class="active"><a href="#">Inicio</a></li>
                   <li><a href="#">Sobre mi</a></li>
-                  <li><a href="#">Contáctos</a></li>
+                  <li><a href="#">Contácto</a></li>
                 </ul>
               </nav>
             </div>
